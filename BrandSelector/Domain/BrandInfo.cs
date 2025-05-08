cde6a7db5a9def14ab9d74640e57af0f49a50bac
bruceyboy24804 <aaronbruce100@gmail.com>
2025-05-08 10:16:06 +0100

    Added Some Unversioned Files

---
+++ BrandSelector/Domain/BrandInfo.cs
@@ -0,0 +1,32 @@
+﻿using Unity.Entities;
+
+namespace BrandSelector.Domain
+{
+    public class BrandInfo
+    {
+        private string m_Name;
+        private Entity m_Entity;
+
+        public string Name
+        {
+            get { return m_Name; }
+            set { m_Name = value; }
+        }
+
+        public Entity Entity
+        {
+            get { return m_Entity; }
+            set { m_Entity = value; }
+        }
+
+        public BrandInfo()
+        {
+        }
+
+        public BrandInfo(string name, Entity entity)
+        {
+            m_Name = name;
+            m_Entity = entity;
+        }
+    }
+}
---
+++ BrandSelector/UI/src/mods/Domain/BrandInfo.ts
@@ -0,0 +1,8 @@
+import { Entity } from "cs2/utils";
+
+// Define the BrandInfo interface to match the C# struct
+export interface BrandInfo {
+    Name: string;
+    Entity: Entity;
+  }
+  
